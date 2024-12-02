66ca343fb80e726245ab5a19ab79cb18a834a2f4
shishodiasaransh <suryarana21122003@gmail.com>
2024-12-02 23:45:38 +0530

    nj

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Allows access from Render's environment
+    port: process.env.PORT || 3000, // Use the PORT environment variable
+  },
 })
